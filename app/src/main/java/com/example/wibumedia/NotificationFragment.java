00da1ca06623bf475c545f51dd86134d85956304
thuantran1704 <74866371+thuantran1704@users.noreply.github.com>
2021-06-11 18:09:13 +0700

    Missing Listview in NotificationFragment

--- app/src/main/java/com/example/wibumedia/NotificationFragment.java
+++ app/src/main/java/com/example/wibumedia/NotificationFragment.java
@@ -2,14 +2,23 @@ package com.example.wibumedia;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class NotificationFragment extends Fragment {
+import com.example.wibumedia.Retrofit.ApiInterface;
+import com.example.wibumedia.Retrofit.Common;
 
+public class NotificationFragment extends Fragment {
+    View root;
+    ApiInterface service;
+    private final String key = "VSBG";
 
     public NotificationFragment() {
         // Required empty public constructor
@@ -20,6 +29,27 @@ public class NotificationFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notification, container, false);
+        root = inflater.inflate(R.layout.fragment_notification, container, false);
+        return root;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        service = Common.getGsonService();
+        setControl(view);
+        setEvent();
+    }
+
+
+
+    private void setControl(View view) {
+        Toolbar toolbar = view.findViewById(R.id.toolbar);
+        if (getActivity() != null) {
+            ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+        }
+    }
+
+    private void setEvent() {
     }
 }
